432e21360fd4db39b9991edb6689cc50bbee100f
Vinicius Ferreira Raymundo <vinicius.raymundo@willbank.com.br>
2022-09-28 18:42:17 -0300

    Last version

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ alunos = [
     }
 ]
 
-aluno = {}
-aluno["nome"] = input("Digite o nome")
-aluno["idade"] = input("Digite a idade")
-alunos.append(aluno)
+for x in range(int(input("Quantos alunos serão cadastros\n"))):
+    aluno = {}
+    aluno["nome"] = input("Digite o nome")
+    aluno["idade"] = input("Digite a idade")
+    aluno["telefo"] = input("digite o telefone")
+    alunos.append(aluno)
 
 for aluno in alunos:
     print(aluno)
