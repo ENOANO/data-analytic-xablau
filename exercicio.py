12c96a26b7d7a38213a48a8809f7814c70b734a4
Vinicius Ferreira Raymundo <vinicius.raymundo@willbank.com.br>
2022-11-03 19:00:54 -0300

    Adicionar estrutura de repeticao

--- exercicio.py
+++ exercicio.py
@@ -1,59 +1,55 @@
 import os
 import time
 
+# Faça com que cada opção solicite o nome e senha da conta para realizar as operações em uma 
+# conta especifica
+
 contas = []
-"""
-check if the solution below is better than the one on line 17
-conta = {}
 
-for conta in contas: 
-    usuario = "Usuario"
-    saldo = "Saldo"
-"""
-os.system('clear')
-usuario = input("Digite o nome do usuario: ")
-deposito = float(input("Digite o valor do deposito inicial: "))
-saldo = deposito
-conta = {"Usuario": usuario, "Saldo": saldo}
-contas.append(conta)
-print (conta)
-time.sleep(2)
-os.system('clear')
+controle = True
+while controle:
+        
+    print ("MENU PRINCIPAL \n")
+    print ("1 - Saldo")
+    print ("2 - Deposito")
+    print ("3 - Saque")
+    print ("4 - Cadastrar novo usuario")
+    print ("5 - Para sair")
 
-print ("MENU PRINCIPAL \n")
-print ("1 - Saldo")
-print ("2 - Deposito")
-print ("3 - Saque")
-print ("4 - Cadastrar novo usuario")
 
-opcao_menu = input("Digite a opcao desejada: ")
+    opcao_menu = input("Digite a opcao desejada: ")
 
-if opcao_menu == "1":
-    print ("Saldo atual: " + str(conta["Saldo"]))
+    if opcao_menu == "1":
+        if contas:
+            print ("Saldo atual: " + str(conta["Saldo"]))
 
-if opcao_menu == "2":
-    deposito = float(input("Digite o valor do deposito: "))
-    conta["Saldo"]=saldo+deposito
-    print ("Deposito efetuado com sucesso!")
-    print ("Saldo atual: " + str(conta["Saldo"]))
-
-if opcao_menu == "3":
-    saque = float(input("Digite o valor a sacar: "))
-    if saque > saldo:
-        print ("Saldo insuficiente!")
-    else:
-        conta["Saldo"]=saldo-saque
-        print ("Saque efetuado com sucesso!")
+    elif opcao_menu == "2":
+        deposito = float(input("Digite o valor do deposito: "))
+        conta["Saldo"]=saldo+deposito
+        print ("Deposito efetuado com sucesso!")
         print ("Saldo atual: " + str(conta["Saldo"]))
 
-if opcao_menu == "4":
-    usuario = input("Digite o nome do usuario: ")
-    deposito = float(input("Digite o valor do deposito inicial: "))
-    saldo = deposito
-    conta = {"Usuario": usuario, "Saldo": saldo}
-    contas.append(conta)
-    print ("Conta cadastrada com sucesso!")
-    print (contas)
+    elif opcao_menu == "3":
+        saque = float(input("Digite o valor a sacar: "))
+        if saque > saldo:
+            print ("Saldo insuficiente!")
+        else:
+            conta["Saldo"]=conta["Saldo"]-saque
+            print ("Saque efetuado com sucesso!")
+            print ("Saldo atual: " + str(conta["Saldo"]))
 
-else:
-    print ("Opcao invalida!")
+    elif opcao_menu == "4":
+        usuario = input("Digite o nome do usuario: ")
+        deposito = float(input("Digite o valor do deposito inicial: "))
+        saldo = deposito
+        conta = {"Usuario": usuario, "Saldo": saldo}
+        contas.append(conta)
+        print ("Conta cadastrada com sucesso!")
+        print (contas)
+    
+    elif opcao_menu == '5':
+        controle = False
+        print("*************************\n Adeus ao Banco Comuna \n *****************")
+
+    else:
+        print ("Opcao invalida!")
---
+++ repeticao.py
@@ -0,0 +1,8 @@
+
+n = 0
+while n < 10:
+    print(n)
+    n = n + 1
+
+for x in range(10):
+    print(x)
